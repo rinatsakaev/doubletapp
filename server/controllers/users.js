efff7611347505ecfa158c68b6b99620fff8e1ba
rinatsakaev <r.sakaevv@gmail.com>
2020-07-14 11:04:18 +0500

    wrapper

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,41 +1,25 @@
 import User from '../models/User';
 
 export async function getUsers(req, res) {
-    try {
-        const users = await User.findAll();
-        res.json(users);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+    const users = await User.findAll();
+    res.json(users);
 }
 
 export async function createUser(req, res) {
-    try{
-        await User.create(req.body);
-        res.sendStatus(200);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+    await User.create(req.body);
+    res.sendStatus(200);
 }
 
 export async function deleteUser(req, res) {
-    try {
-        const user = await User.findByPk(req.params.id);
-        if (!user)
-            res.sendStatus(404);
+    const user = await User.findByPk(req.params.id);
+    if (!user)
+        res.sendStatus(404);
 
-        await user.destroy();
-        res.sendStatus(200);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+    await user.destroy();
+    res.sendStatus(200);
 }
 
 export async function getUser(req, res) {
-    try {
-        const user = await User.findByPk(req.params.id);
-        res.json(user);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
 }
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,14 @@
 import {getUsers, getUser, createUser, deleteUser} from './controllers/users';
-
+async function errorWrapper(req, res, func) {
+    try{
+        await func(req, res);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+}
 export default (app) => {
-    app.get('/users/', getUsers);
-    app.get('/users/:id', getUser);
-    app.delete('/users/:id', deleteUser);
-    app.post('/users/create', createUser);
+    app.get('/users/', (req, res) => errorWrapper(req, res, getUsers));
+    app.get('/users/:id', (req, res) => errorWrapper(req, res, getUser));
+    app.delete('/users/:id', (req, res) => errorWrapper(req, res, deleteUser));
+    app.post('/users/create', (req, res) => errorWrapper(req, res, createUser));
 };

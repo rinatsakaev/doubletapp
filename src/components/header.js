1300e2003270525aecda726f4edae84a0d7c7268
rinatsakaev <r.sakaevv@gmail.com>
2020-07-31 20:51:49 +0500

    fix header

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,11 @@ import {Link} from 'react-router-dom';
 export default function Header() {
     return (
         <Link to="/">
-
             <div className="header">
                 <div className="header__logo"/>
                 <span className="header__title">Students</span>
+                <a className="header__profile" href="https://github.com/rinatsakaev"
+                   onClick={() => window.open("https://github.com/rinatsakaev/doubletapp")}>rinatsakaev</a>
             </div>
         </Link>
     );
